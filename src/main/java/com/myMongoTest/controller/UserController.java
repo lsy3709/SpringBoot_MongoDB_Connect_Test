b7da1b05d1230622a8cc589128d4411dca06f0c0
이상용 <lsy3709@naver.com>
2023-08-07 09:45:51 +0900

    저장 테스트2

--- src/main/java/com/myMongoTest/controller/UserController.java
+++ src/main/java/com/myMongoTest/controller/UserController.java
@@ -36,7 +36,7 @@ public class UserController {
 	        model.addAttribute("loginErrorMsg", "아이디 또는 비밀번호를 확인해주세요");
 	        return "loginForm";
 	    }
-	  
+	  //test
 	    @GetMapping(value = "/login")
 	    public String loginMember(){
 	        return "loginForm";
